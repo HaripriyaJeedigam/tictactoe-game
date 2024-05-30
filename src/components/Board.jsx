31b9682d7af9164dd028540fa446d19031171aab
Haripriya <haripriyaj2002@gmail.com>
2024-05-30 15:38:50 +0530

    lifted up the state to App component.Show status message to use, calculate winner

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,6 @@
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
 import Square from './Square';
-const Board = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-  // eslint-disable-next-line no-unused-vars
-  const handleSquareClick = clickedPosition => {
-    if (squares[clickedPosition]) {
-      return;
-    }
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isXNext ? 'X' : 'O';
-        }
-        return squareValue;
-      });
-    });
-    setIsXNext(currentIsXNext => !currentIsXNext);
-  };
-  // eslint-disable-next-line no-unused-vars
+const Board = ({ squares, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
